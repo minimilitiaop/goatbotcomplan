fb48294d6a4e631208a2fb3c77b40b9e637f51f2
Tuxedoun <163103023+Tuxedoun@users.noreply.github.com>
2024-03-31 15:58:00 +0800

    Update pinterest.js

--- scripts/cmds/pinterest.js
+++ scripts/cmds/pinterest.js
@@ -1,95 +1,70 @@
-const axios = require('axios');
-const fs = require('fs-extra');
+const axios = require("axios");
+const path = require("path");
+
+/*Do not change
+        the credit 🐢👑*/
 
 module.exports = {
   config: {
-    name: 'Pinterest',
-    aliases: ["pint", "pinter"],
-    version: '1.2',
-    author: 'Samuel',
-    countDown: 5,
+    name: "pinterest",
+    aliases: ["pin"],
+    version: "1.0",
+    author: "rehat--",
     role: 0,
-    category: 'Image Search',
-    shortDescription: {
-      en: "Search for images on Pinterest based on a keyword",
-    },
+    countDown: 60,
     longDescription: {
-      en: "This command searches for images on Pinterest based on a provided keyword.",
+      en: "This command allows you to search for images on Pinterest based on a given query and fetch a specified number of images."
     },
+    category: "image",
     guide: {
-      en: "{pn} 'keyword' -'number of search results'\nExample: {pn} cute -10\nIf no number is provided, the command will return the first 5 images.",
-    },
+      en: "{pn} <search query> <number of images>\nExample: {pn} Tomozaki -10"
+    }
   },
 
-  onStart: async function ({ api, args, event , message }) {
-    const { getPrefix } = global.utils;
-       const p = getPrefix(event.threadID);
-    const approvedmain = JSON.parse(fs.readFileSync(`${__dirname}/assist_json/approved_main.json`));
-    const bypassmain = JSON.parse(fs.readFileSync(`${__dirname}/assist_json/bypass_id.json`));
-    const bypassmUid = event.senderID;
-    if (bypassmain.includes(bypassmUid)) {
-      console.log(`User ${bypassmUid} is in bypass list. Skipping the main approval check.`);
-    } else {
-      const threadmID = event.threadID;
-      if (!approvedmain.includes(threadmID)) {
-        const msgSend = message.reply(`cmd 'Pinterest' is locked 🔒...\n Reason : Bot's main cmd \nyou need permission to use all main cmds.\n\nType ${p}requestMain to send a request to admin`);
-        setTimeout(async () => {
-          message.unsend((await msgSend).messageID);
-        }, 40000);
-        return;
-      }
-    }  
-                                                  
-
-
-
-    
-    let keyword = args.join(' ');
-    let numberSearch = 4;
-    const match = keyword.match(/(.+?)\s*-?(\d+)?$/);
-    if (match) {
-      keyword = match[1].trim();
-      if (match[2]) {
-        numberSearch = parseInt(match[2]);
-      }
-    }
-
-    if (!keyword) {
-      api.sendMessage("Please provide a keyword.\nExample: Pinterest cute anime boy -10", event.threadID, event.messageID);
-      return;
-    }
-
-    if (numberSearch > 20) {
-      api.sendMessage("Maximum number of search results is 20.", event.threadID, event.messageID);
-      return;
-    }
-
+  onStart: async function ({ api, event, args }) {
     try {
-      const res = await axios.get(`https://turtle-apis.onrender.com/api/pinterest?search=${encodeURIComponent(keyword)}`);
-      const data = res.data.data;
-      let num = 0;
-      const img = [];
-
-      for (let i = 0; i < numberSearch; i++) {
-        const path = __dirname + `/tmp/${num += 1}.jpg`;
-        const getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
-        fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-        img.push(fs.createReadStream(path));
+      const fs = require("fs-extra");
+      const keySearch = args.join(" ");
+      if (!keySearch.includes("-")) {
+        return api.sendMessage(
+          "Please enter the search query and number of images (1-10)",
+          event.threadID,
+          event.messageID
+        );
+      }
+      const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
+      let numberSearch = keySearch.split("-").pop() || 10;
+      if (numberSearch > 10) {
+        numberSearch = 10;
       }
 
-      api.sendMessage({
-        body: `${numberSearch} search results for keyword: ${keyword}`,
-        attachment: img
+      const apiUrl = `https://turtle-apis.onrender.com/api/pinterest?search=${encodeURIComponent(keySearchs)}&keysearch=${numberSearch}`;
+
+      const res = await axios.get(apiUrl);
+      const data = res.data.images;
+      const imgData = [];
+
+      for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
+        const imgResponse = await axios.get(data[i], {
+          responseType: "arraybuffer"
+        });
+        const imgPath = path.join(__dirname, "cache", `${i + 1}.jpg`);
+        await fs.outputFile(imgPath, imgResponse.data);
+        imgData.push(fs.createReadStream(imgPath));
+      }
+
+      await api.sendMessage({
+        attachment: imgData,
       }, event.threadID, event.messageID);
 
-      for (let ii = 1; ii < numberSearch; ii++) {
-        fs.unlinkSync(__dirname + `/tmp/${ii}.jpg`);
-      }
-    } catch (err) {
-      console.error(err);
-      api.sendMessage("Your search Input in Disallowed in Pinterest.", event.threadID, event.messageID);
-      return;
+      await fs.remove(path.join(__dirname, "cache"));
+    } catch (error) {
+      console.error(error);
+      return api.sendMessage(
+        `An error occurred.`,
+        event.threadID,
+        event.messageID
+      );
     }
   }
 };
-                 
